d9b96e2128650b395383806399ed8896f58d23bb
Rafael Araujo <rafa123839@gmail.com>
2023-04-21 09:01:13 -0300

    Title dinâmico por página

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
                     <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
-                    <title>ig.news</title>
+
                 </Head>
                 <body>
                     <Main/>
