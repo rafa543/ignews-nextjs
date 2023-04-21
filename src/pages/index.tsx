16b1171052071af819a933ea3c8e6c3542108137
Rafael Araujo <rafa123839@gmail.com>
2023-04-21 15:50:58 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from './home.module.scss'
 import { SubscribeButton } from '@/components/SubscribeButton'
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, GetStaticProps } from 'next'
 import { stripe } from '@/services/stripe'
 
 interface HomeProps {
@@ -36,7 +36,7 @@ export default function Home({product} : HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1MzOMjFzR3ErZ7Dn31hH08eY')
 
   const product = {
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 *24
   }
 }
