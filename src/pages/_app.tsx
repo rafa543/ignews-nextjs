ee964cdd707ffed3bcfd5fb016f0aeefc2ed995e
Rafael Araujo <rafa123839@gmail.com>
2023-04-21 09:41:52 -0300

    Componente: Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../styles/global.scss'
+import { Header } from '@/components/Header'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
