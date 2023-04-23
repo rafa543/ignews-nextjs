f0c911eb36a7530b6968ea03a87327646a2b4820
Rafael Araujo <rafa123839@gmail.com>
2023-04-23 09:45:52 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,38 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProviders from 'next-auth/providers/github';
 
+import { fauna } from '@/services/fauna';
+
 export default NextAuth({
-    
+
     providers: [
         GithubProviders({
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
             authorization: {
                 params: {
-                  scope: 'read:user',
+                    scope: 'read:user',
                 },
-              },
+            },
         })
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        { data: { email } }
+                    )
+                )
+                return true
+            } catch (error) {
+                return false
+            }
+
+        },
+    }
 });
