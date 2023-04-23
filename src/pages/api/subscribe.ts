33f91f103e8bb4db1741215e812a104f8bf665b3
Rafael Araujo <rafa123839@gmail.com>
2023-04-23 15:55:04 -0300

    Evitando duplicação no Stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from 'faunadb'
 import { getSession } from "next-auth/react";
 import { stripe } from "../../services/stripe";
+import { fauna } from "@/services/fauna";
+
+type User = {
+    ref: {
+        id: string
+    }
+    data: {
+        stripe_customer_id: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
@@ -12,12 +23,39 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             })
         }
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
+        let customerId = user.data.stripe_customer_id
+
+        if (!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+            })
+
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id
+        }
 
         const checkoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             billing_address_collection: 'required',
             line_items: [
                 { price: 'price_1MzOMjFzR3ErZ7Dn31hH08eY', quantity: 1 }
